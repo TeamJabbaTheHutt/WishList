a7e1d3bb1eca1b56a7a6f8f5dcabedda30d2850d
Ella Costello <ella.costello@yahoo.com>
2025-10-29 09:56:02 +0100

    commit for merge from main

--- src/main/java/com/hauxy/wishlist/controller/LoginController.java
+++ src/main/java/com/hauxy/wishlist/controller/LoginController.java
@@ -1,7 +1,21 @@
 package com.hauxy.wishlist.controller;
 
+import com.hauxy.wishlist.service.LoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    private LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @GetMapping()
+    public String login(Model model) {
+        model.addAttribute("email", email);
+        model.addAttribute("password", password);
+    }
 }
--- src/main/java/com/hauxy/wishlist/service/LoginService.java
+++ src/main/java/com/hauxy/wishlist/service/LoginService.java
@@ -1,7 +1,11 @@
 package com.hauxy.wishlist.service;
 
+import com.hauxy.wishlist.model.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
+    private User user;
+
+
 }
