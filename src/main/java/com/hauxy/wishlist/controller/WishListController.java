09126c21fcf38867845a12e0ca0016c05ac1a05a
Ella Costello <ella.costello@yahoo.com>
2025-11-03 11:26:31 +0100

    hh

--- src/main/java/com/hauxy/wishlist/controller/WishListController.java
+++ src/main/java/com/hauxy/wishlist/controller/WishListController.java
@@ -1,7 +1,23 @@
 package com.hauxy.wishlist.controller;
 
+import com.hauxy.wishlist.model.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WishListController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(HttpSession httpSession, Model model) {
+        User user = (User) httpSession.getAttribute("loggedInUser");
+
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("user", user);
+        return "dashboard";
+    }
 }
